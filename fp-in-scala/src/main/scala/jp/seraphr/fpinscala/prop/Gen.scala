148226be586c2c5376173a4416e127cb8fb7a5c7
seraph <seraph.sub+github@gmail.com>
2015-07-27 05:00:14 +0900

    EXERCISE 8.11

--- fp-in-scala/src/main/scala/jp/seraphr/fpinscala/prop/Gen.scala
+++ fp-in-scala/src/main/scala/jp/seraphr/fpinscala/prop/Gen.scala
@@ -5,6 +5,7 @@ import jp.seraphr.fpinscala.state.{ State, RNG }
 /**
  */
 case class Gen[A](sample: State[RNG, A]) {
+  def map[B](f: A => B): Gen[B] = Gen(sample.map(f))
   def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
   def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(Gen.listOfN(_, this))
   def unsized: SGen[A] = SGen(_ => this)
