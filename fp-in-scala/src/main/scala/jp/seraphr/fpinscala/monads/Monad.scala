825e57182aeef8b47a61d97786fcca47499d0af6
seraph <seraph.sub+github@gmail.com>
2017-04-24 03:22:00 +0900

    EXERCISE 11.12

--- fp-in-scala/src/main/scala/jp/seraphr/fpinscala/monads/Monad.scala
+++ fp-in-scala/src/main/scala/jp/seraphr/fpinscala/monads/Monad.scala
@@ -38,6 +38,7 @@ trait Monad[M[_]] extends Functor[M] {
   }
 
   def flatMapViaCompose[A, B](ma: M[A])(f: A => M[B]): M[B] = compose[M[A], A, B](identity, f)(ma)
+  def join[A](mma: M[M[A]]): M[A] = flatMap(mma)(identity)
 }
 
 object Monad {
